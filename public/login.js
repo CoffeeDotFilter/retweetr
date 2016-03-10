a0edd7d83e2d105e080ec697ca5f5cd12157627a
Owen TM <oturnermajor@gmail.com>
2016-03-10 17:34:23 +0000

    changed order in login.js

--- public/login.js
+++ public/login.js
@@ -1,25 +1,21 @@
 var swipeRight = document.getElementById('swipe-right');
 var hammerRight = new Hammer(swipeRight, {});
-
-var swipeUp = document.getElementById('swipe-up');
-var hammerUp = new Hammer(swipeUp, {});
-hammerUp.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
-
-var swipeLeft = document.getElementById('swipe-left');
-var hammerLeft = new Hammer(swipeLeft, {});
-
-
 hammerRight.on('swiperight', function() {
 	swipeRight.style.transform = 'translateX(800px)';
 	console.log('swiped right!');
 });
 
-hammerLeft.on('swipeleft', function() {
-	swipeLeft.style.transform = 'translateX(-800px)';
-	console.log('swiped left!');
-});
-
+var swipeUp = document.getElementById('swipe-up');
+var hammerUp = new Hammer(swipeUp, {});
+hammerUp.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 hammerUp.on('swipeup', function() {
 	swipeUp.style.transform = 'translateY(-800px)';
 	console.log('swiped up!');
+});
+
+var swipeLeft = document.getElementById('swipe-left');
+var hammerLeft = new Hammer(swipeLeft, {});
+hammerLeft.on('swipeleft', function() {
+	swipeLeft.style.transform = 'translateX(-800px)';
+	console.log('swiped left!');
 });
